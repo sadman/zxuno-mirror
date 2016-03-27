f8c24d0f114686a94224ef625655c0b347ff51fe
rodriguj <rodriguj@zxuno>
2016-03-27 22:31:51 +0000

    git-svn-id: http://www.atc.us.es/svn/zxuno@854 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

---
+++ cores/jupiter_ace_spartan6/test4_az80/tld_jace_spartan6.v
@@ -0,0 +1,123 @@
+`timescale 1ns / 1ps
+`default_nettype none
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:18:12 11/07/2015 
+// Design Name: 
+// Module Name:    tld_jace_spartan6 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module tld_jace_spartan6 (
+    input wire clk50mhz,
+    input wire clkps2,
+    input wire dataps2,
+    input wire ear,
+    output wire audio_out_left,
+    output wire audio_out_right,
+    output wire [2:0] r,
+    output wire [2:0] g,
+    output wire [2:0] b,
+    output wire hsync,
+	output wire vsync,
+    output wire stdn,
+    output wire stdnb
+    );
+    
+    wire clkram; // 26.666666MHz to clock internal RAM/ROM
+    wire clk65;  // 6.5MHz main frequency Jupiter ACE
+    wire clkcpu; // CPU CLK
+	 wire clkvga; // Twice the original pixel clock
+    
+    wire kbd_reset;
+    wire [7:0] kbd_rows;
+    wire [4:0] kbd_columns;
+    wire video; // 1-bit video signal (black/white)
+    
+    // Trivial conversion from B/W video to RGB for the scandoubler
+	 wire pal_hsync, pal_vsync; // inputs to the scandoubler
+    wire [2:0] ri = {video,video,1'b0};
+    wire [2:0] gi = {video,video,1'b0};
+    wire [2:0] bi = {video,video,1'b0};
+    
+    // Trivial conversion for audio
+    wire mic,spk;
+    assign audio_out_left = spk;
+    assign audio_out_right = mic;
+    
+    // Select PAL
+    assign stdn = 1'b0;  // PAL selection for AD724
+    assign stdnb = 1'b1;  // 4.43MHz crystal selected
+    
+    // Power-on RESET (8 clocks)
+    reg [7:0] poweron_reset = 8'h00;
+    always @(posedge clk65)
+        poweron_reset <= {poweron_reset[6:0],1'b1};
+    
+    cuatro_relojes system_clocks_pll (
+        .CLK_IN1(clk50mhz),
+        .CLK_OUT1(clkram),  // for driving synch RAM and ROM = 26.6666 MHz
+        .CLK_OUT2(clk65),   // video clock = 6.66666 MHz
+        .CLK_OUT3(clkcpu),  // CPU clock = 0.5 video clock
+        .CLK_OUT4(clkvga)   // VGA clock: 2 x video clock
+    );
+    
+    jupiter_ace the_core (
+        .clkram(clkram),
+        .clk65(clk65),
+        .clkcpu(clkcpu),
+        .reset(kbd_reset & poweron_reset[7]),
+        .ear(ear),
+        .filas(kbd_rows),
+        .columnas(kbd_columns),
+        .video(video),
+        .hsync(pal_hsync),
+		  .vsync(pal_vsync),
+        .mic(mic),
+        .spk(spk)
+	);
+
+    keyboard_for_ace the_keyboard (
+        .clk(clk65),
+        .clkps2(clkps2),
+        .dataps2(dataps2),
+        .rows(kbd_rows),
+        .columns(kbd_columns),
+        .kbd_reset(kbd_reset),
+        .kbd_nmi()        
+    );
+	 
+    assign r = ri;
+    assign g = gi;
+    assign b = bi;
+    assign hsync = pal_hsync & pal_vsync; // composite sync
+    assign vsync = 1'b1;    
+     
+//	vga_scandoubler #(.CLKVIDEO(6667)) salida_vga (
+//		.clkvideo(clk65),
+//		.clkvga(clkvga),
+//        .disable_scaneffect(1'b1),
+//		.ri(ri),
+//		.gi(gi),
+//		.bi(bi),
+//		.hsync_ext_n(pal_hsync),
+//		.vsync_ext_n(pal_vsync),
+//		.ro(r),
+//		.go(g),
+//		.bo(b),
+//		.hsync(hsync),
+//		.vsync(vsync)
+//   );	 
+
+endmodule
---
+++ cores/jupiter_ace_spartan6/test4_az80/tv80_to_t80_wrapper.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    03:39:55 05/13/2012 
+// Design Name: 
+// Module Name:    tv80_to_t80_wrapper 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module tv80n_wrapper (
+  // Outputs
+  m1_n, mreq_n, iorq_n, rd_n, wr_n, rfsh_n, halt_n, busak_n, A, dout,
+  // Inputs
+  reset_n, clk, wait_n, int_n, nmi_n, busrq_n, di
+  );
+
+  input         reset_n; 
+  input         clk; 
+  input         wait_n; 
+  input         int_n; 
+  input         nmi_n; 
+  input         busrq_n; 
+  output        m1_n; 
+  output        mreq_n; 
+  output        iorq_n; 
+  output        rd_n; 
+  output        wr_n; 
+  output        rfsh_n; 
+  output        halt_n; 
+  output        busak_n; 
+  output [15:0] A;
+  input [7:0]   di;
+  output [7:0]  dout;
+
+  wire [7:0] d;
+
+  z80_top_direct_n TheCPU (
+  		.nRESET(reset_n),
+		.CLK(clk),
+		.nWAIT(wait_n),
+		.nINT(int_n),
+		.nNMI(nmi_n),
+		.nBUSRQ(busrq_n),
+		.nM1(m1_n),
+		.nMREQ(mreq_n),
+		.nIORQ(iorq_n),
+		.nRD(rd_n),
+		.nWR(wr_n),
+		.nRFSH(rfsh_n),
+		.nHALT(halt_n),
+		.nBUSACK(busak_n),
+		.A(A),
+		.D(d)
+	);
+	
+	assign dout = d; 
+	assign d = ( (!mreq_n || !iorq_n) && !rd_n)? di : 
+               ( (!mreq_n || !iorq_n) && !wr_n)? 8'hZZ :
+               8'hFF;
+
+endmodule
