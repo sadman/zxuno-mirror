00c618073ed6999d9f16f9a3ea1b8348fe151499
rodriguj <rodriguj@zxuno>
2016-03-27 16:25:00 +0000

    git-svn-id: http://www.atc.us.es/svn/zxuno@850 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- cores/cores_para_testar_la_placa/test_pal_ntsc/dummy_ula.v
+++ cores/cores_para_testar_la_placa/test_pal_ntsc/dummy_ula.v
@@ -28,12 +28,12 @@ module dummy_ula(
     output wire csync
     );
 
-	 reg [2:0] ri = 3'b000;
-	 reg [2:0] gi = 3'b000;
-	 reg [2:0] bi = 3'b000;
-	 wire [8:0] hc;
-	 wire [8:0] vc;
-     wire blank;
+    reg [2:0] ri = 3'b000;
+	reg [2:0] gi = 3'b000;
+	reg [2:0] bi = 3'b000;
+	wire [8:0] hc;
+	wire [8:0] vc;
+    wire blank;
 
     sync_generator_pal_ntsc sincronismos (
     .clk(clk),   // 7 MHz
@@ -44,55 +44,44 @@ module dummy_ula(
     .blank(blank)
     );
     
+    parameter BLACK   = 9'b000000000,
+              BLUE    = 9'b000000111,
+              RED     = 9'b000111000,
+              MAGENTA = 9'b000111111,
+              GREEN   = 9'b111000000,
+              CYAN    = 9'b111000111,
+              YELLOW  = 9'b111111000,
+              WHITE   = 9'b111111111;
+    
     always @* begin
         if (blank == 1'b1) begin
-            r = 3'b000;
-            g = 3'b000;
-            b = 3'b000;
+            {g,r,b} = BLACK;
         end
         else begin
-            r = 3'b000;
-            g = 3'b000;
-            b = 3'b000;
+            {g,r,b} = BLACK;
             if (hc >= (44*0) && hc <= (44*1-1)) begin
-                r = 3'b000;
-                g = 3'b000;
-                b = 3'b000;
+                {g,r,b} = WHITE;
             end
             if (hc >= (44*1) && hc <= (44*2-1)) begin
-                r = 3'b000;
-                g = 3'b000;
-                b = 3'b111;
+                {g,r,b} = YELLOW;
             end
             if (hc >= (44*2) && hc <= (44*3-1)) begin
-                r = 3'b111;
-                g = 3'b000;
-                b = 3'b000;
+                {g,r,b} = CYAN;
             end
             if (hc >= (44*3) && hc <= (44*4-1)) begin
-                r = 3'b111;
-                g = 3'b000;
-                b = 3'b111;
+                {g,r,b} = GREEN;
             end
             if (hc >= (44*4) && hc <= (44*5-1)) begin
-                r = 3'b000;
-                g = 3'b111;
-                b = 3'b000;
+                {g,r,b} = MAGENTA;
             end
             if (hc >= (44*5) && hc <= (44*6-1)) begin
-                r = 3'b000;
-                g = 3'b111;
-                b = 3'b111;
+                {g,r,b} = RED;
             end
             if (hc >= (44*6) && hc <= (44*7-1)) begin
-                r = 3'b111;
-                g = 3'b111;
-                b = 3'b000;
+                {g,r,b} = BLUE;
             end
             if (hc >= (44*7) && hc <= (44*8-1)) begin
-                r = 3'b111;
-                g = 3'b111;
-                b = 3'b111;
+                {g,r,b} = BLACK;
             end
         end
     end        
