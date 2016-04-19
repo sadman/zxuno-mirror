fa13830d75fb666b8c20753e44eea3cefb7b484f
avillena <avillena@zxuno>
2016-04-19 21:34:34 +0000

    bootloader.asm depurado
    git-svn-id: http://www.atc.us.es/svn/zxuno@907 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- firmware/bootloader.asm
+++ firmware/bootloader.asm
@@ -9,7 +9,7 @@
         define  master_mapper   1
         define  flash_spi       2
         define  flash_cs        3
-        define  joystick        6
+        define  joyconf         6
         di
         ld      bc, zxuno_port + $100
         wreg    flash_cs, 1     ; desactivamos spi, enviando un 0
@@ -19,7 +19,7 @@
         jr      cont
 
 ldedg2  rst     $18             ; call routine ld-edge-1 below.
-        ret     nc              ; return if space pressed or time-out.
+        ret     z               ; return if space pressed or time-out.
         jr      ldedg1
         defb    '2016'
 
@@ -28,7 +28,6 @@ ldelay  dec     a               ; decrement counter
         jr      nz, ldelay      ; loop back to ld-delay 22 times.
 lsampl  inc     b               ; increment the time-out counter.
         ret     z               ; return with failure when $ff passed.
-        dec     a               ; prepare to read keyboard and ear port
         in      a, ($fe)        ; row $7ffe. bit 6 is ear, bit 0 is space key.
         rra
         xor     c               ; compare with initial long-term state.
@@ -38,8 +37,9 @@ lsampl  inc     b               ; increment the time-out counter.
         xor     $27             ; switch the bits
         ld      c, a            ; and put back in c for long-term.
         out     ($fe), a        ; send to port to effect the change of colour. 
-        scf                     ; set carry flag signaling edge found within time allowed
         ret                     ; return.
+        nop
+        nop
 
 rst30   pop     hl
         outi
@@ -62,13 +62,13 @@ boot    ini
         inc     b
         cp      h               ; compruebo si la direccion es 0000 (final)
         jr      c, boot         ; repito si no lo es
-        wreg    joystick, %00100000     ; desactivamos spi, enviando un 0
+        wreg    joyconf, %00010000
         dec     b
         out     (c), h          ; a master_conf quiero enviar un 0 para pasar
         in      a, ($1f)
         or      %11100111       ; arriba y disparo a la vez
-        inc     a
         inc     b
+        inc     a
         jr      nbreak
         nop
         nop
@@ -79,26 +79,26 @@ nmi66   jp      $c000
 nbreak  ld      de, $bffc-61
         push    de
         ret     nz
-        ld      de, $0051
+        ld      de, $0051+2
         ld      ixh, e
         call    lbytes
         ld      ix, $c000
-        ld      d, $40
+        ld      de, $4000+2
 lbytes  ld      a, $0f          ; make the border white and mic off.
         out     ($fe), a        ; output to port.
-        ld      c, 2
+        ld      c, 10
 lstart  rst     $18             ; routine ld-edge-1
-        jr      nc, lstart      ; back to ld-break with time out and no edge present on tape
+        jr      z, lstart       ; back to ld-break with time out and no edge present on tape
         xor     a               ; set up 8-bit outer loop counter for approx 0.45 second delay
 ldwait  add     hl, hl
         djnz    ldwait          ; self loop to ld-wait (for 256 times)
         dec     a               ; decrease outer loop counter.
         jr      nz, ldwait      ; back to ld-wait, if not zero, with zero in b.
         rst     $10             ; routine ld-edge-2
-        jr      nc, lstart      ; back to ld-break if no edges at all.
+        jr      z, lstart       ; back to ld-break if no edges at all.
 leader  ld      b, $9c          ; two edges must be spaced apart.
         rst     $10             ; routine ld-edge-2
-        jr      nc, lstart      ; back to ld-break if time-out
+        jr      z, lstart       ; back to ld-break if time-out
         ld      a, $c6          ; two edges must be spaced apart.
         cp      b               ; compare
         jr      nc, lstart      ; back to ld-start if too close together for a lead-in.
@@ -106,28 +106,21 @@ leader  ld      b, $9c          ; two edges must be spaced apart.
         jr      nz, leader      ; back to ld-leader while more to do.
 ldsync  ld      b, $c9          ; two edges must be spaced apart.
         rst     $18             ; routine ld-edge-1
-        jr      nc, lstart      ; back to ld-break with time-out.
+        jr      z, lstart       ; back to ld-break with time-out.
         ld      a, b            ; fetch augmented timing value from b.
         cp      $d4             ; compare 
         jr      nc, ldsync      ; back to ld-sync if gap too big, that is, a normal lead-in edge gap
         rst     $18             ; routine ld-edge-1
-        ld      a, c
+        jr      z, binf
         xor     3
         ld      c, a
-        jr      marker          ; forward to ld-marker 
-ldloop  ex      af, af'         ; restore entry flags and type in a.
-        jr      nz, ldflag      ; forward to ld-flag if awaiting initial flag, to be discarded
-        ld      (ix), l         ; place loaded byte at memory location.
+ldloop  ld      (ix+2), l       ; place loaded byte at memory location.
         inc     ix              ; increment byte pointer.
         dec     de              ; decrement length.
-        defb    $c2
-ldflag  inc     l               ; compare type in a with first byte in l.
-        ret     nz              ; return if no match e.g. code vs. data.
-marker  ex      af, af'         ; store the flags.
         ld      l, $01          ; initialize as %00000001
 l8bits  ld      b, $b2          ; timing.
         rst     $10             ; routine ld-edge-2 increments b relative to gap between 2 edges
-        ret     nc              ; return with time-out.
+binf    jr      z, binf         ; return with time-out.
         ld      a, $cb          ; the comparison byte.
         cp      b               ; compare to incremented value of b.
         rl      l               ; rotate the carry bit into l.
@@ -135,9 +128,11 @@ l8bits  ld      b, $b2          ; timing.
         ld      a, h            ; fetch the running parity byte.
         xor     l               ; include the new byte.
         ld      h, a            ; and store back in parity register.
-        or      d               ; check length of
+        ld      a, d            ; check length of
         or      e               ; expected bytes.
         jr      nz, ldloop      ; back to ld-loop while there are more.
+        or      h
+bin2    jr      nz, bin2
         ld      bc, zxuno_port + $100
         ret                     ; return
 
