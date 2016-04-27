e49c6fff4cf4b89aa4029c32cb770f835464392f
avillena <avillena@zxuno>
2016-04-27 20:56:23 +0000

    Modifico make.bat para que no dé error al compilar
    git-svn-id: http://www.atc.us.es/svn/zxuno@949 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- firmware/make.bat
+++ firmware/make.bat
@@ -2,6 +2,7 @@ rem zx7b      logo256x192.rcs       logo256x192.rcs.zx7b
 rem zx7b      ES  es.zx7b
 rem zx7b      US  us.zx7b
 rem zx7b      AV  av.zx7b
+if not exist strings.bin echo > strings.bin
 sjasmplus firmware.asm
 fcut      firmware_strings.rom  7e00 -7e00  strings.bin
 zx7b      strings.bin           strings.bin.zx7b
