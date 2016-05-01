01ab56c34281d11bda033837f49726078eeed016
avillena <avillena@zxuno>
2016-05-01 12:33:55 +0000

    pongo un 4 en generamcs.bat (copia 4 veces la ROM de 48K) para evitar el bug del reset
    git-svn-id: http://www.atc.us.es/svn/zxuno@965 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- firmware/roms/generamcs.bat
+++ firmware/roms/generamcs.bat
@@ -26,7 +26,7 @@ GenRom 0 0 0 0 0 ESXDOS esxdos.rom core_taps\ESXDOS.TAP
 call :CreateRom 0  "ZX Spectrum 48K Cargando Leches" leches         dn   8  4 0 0
 call :CreateRom 1  "ZX +3e DivMMC"                 plus3en40divmmc  t    8  4 0 0
 call :CreateRom 5  "SE Basic IV 4.0 Anya"          se               d    8  4 0 0
-call :CreateRom 7  "ZX Spectrum 48K"               48               dn   8  1 0 32
+call :CreateRom 7  "ZX Spectrum 48K"               48               dn   8  4 0 32
 call :CreateRom 8  "Jet Pac (1983)"                JetPac           0    8  1 0 32
 call :CreateRom 9  "Pssst (1983)"                  Pssst            0    8  1 0 32
 call :CreateRom 10 "Cookie (1983)"                 Cookie           0    8  1 0 32
