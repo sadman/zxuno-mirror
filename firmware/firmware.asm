2a32c8b60e58266622cf0bd81224d19f27ba5d84
avillena <avillena@zxuno>
2016-05-19 22:31:59 +0000

    Reduzco a 1x en las cargas de cinta para que funcionen
    git-svn-id: http://www.atc.us.es/svn/zxuno@993 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- firmware/firmware.asm
+++ firmware/firmware.asm
@@ -3989,12 +3989,17 @@ l3ec3   ld      a, ixl
         in      l, (c)
         jp      (hl)
 
-        block   $3eff-$         ; 50 bytes
+        block   $3ef5-$         ; 40 bytes
+
+lbytes  wreg    scandbl_ctrl, 0
+        call    lbytes2
+        wreg    scandbl_ctrl, $80
+        ret
 
 l3eff   in      l,(c)
         jp      (hl)
 
-lbytes  di                      ; disable interrupts
+lbytes2 di                      ; disable interrupts
         ld      a, $0f          ; make the border white and mic off.
         out     ($fe), a        ; output to port.
         push    ix
@@ -4162,7 +4167,7 @@ decbhl  dec     hl
         block   $7e00-$
 cad0    defb    'Core:             ',0
 cad1    defm    'http://zxuno.speccy.org', 0
-        defm    'ZX-Uno BIOS v0.329', 0
+        defm    'ZX-Uno BIOS v0.40', 0
         defm    'Copyleft ', 127, ' 2016 ZX-Uno Team', 0
         defm    'Processor: Z80 3.5MHz', 0
         defm    'Memory:    512K Ok', 0
@@ -4197,7 +4202,7 @@ cad8    defm    $10, '                         ', $10, '              ', $10, 0
 cad9    defb    $14, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11
         defb    $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $18, $11
         defb    $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $15, 0
-        defb    '   BIOS v0.329   ', $7f, '2016 ZX-Uno Team', 0
+        defb    '   BIOS v0.40    ', $7f, '2016 ZX-Uno Team', 0
 cad10   defb    'Hardware tests', 0
         defb    $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11, $11
         defb    $11, $11, $11, $11, 0
