3f7b7ba04e146e3283e056cf232bc768fba46804
rodriguj <rodriguj@zxuno>
2016-11-05 18:51:06 +0000

    Commit definitivo del TEST 23, pr evio a comenzar con el TEST 24. Sólo cambia la cadena de identificación del core.
    git-svn-id: http://www.atc.us.es/svn/zxuno@1175 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- cores/spectrum_v2_spartan6/test23/common/ula_radas.v
+++ cores/spectrum_v2_spartan6/test23/common/ula_radas.v
@@ -105,9 +105,9 @@ module ula_radas (
     
     // Initial values for synch, syncv for all supported timings
     reg [8:0] hinit48k = 9'd112;
-    reg [8:0] vinit48k = 9'd2;
+    reg [8:0] vinit48k = 9'd0;
     reg [8:0] hinit128k = 9'd116;
-    reg [8:0] vinit128k = 9'd2;
+    reg [8:0] vinit128k = 9'd0;
     reg [8:0] hinitpen = 9'd110;
     reg [8:0] vinitpen = 9'd0;    
    
