5b47a6e6f67384a876ec8dd55ffedc9362bc8a9c
rodriguj <rodriguj@zxuno>
2017-02-13 18:28:47 +0000

    git-svn-id: http://www.atc.us.es/svn/zxuno@1224 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- cores/spectrum_v2_spartan6/exp25/common/dma.v
+++ cores/spectrum_v2_spartan6/exp25/common/dma.v
@@ -22,23 +22,44 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module dma (
-   input wire clk,
-   input wire rst_n,
-   input wire [7:0] zxuno_addr,
-   input wire zxuno_regrd,
-   input wire zxuno_regwr,
-   input wire [7:0] din,
-   output reg [7:0] dout,
-   output reg oe_n,
-   //---- DMA bus -----
-   output reg busrq_n,
-   input wire busak_n,
-   output reg [15:0] dma_a,
-   output reg dma_mreq_n,
-   output reg dma_iorq_n,
-   output reg dma_rd_n,
-   output reg dma_wr_n   
-   );
+    input wire clk,
+    input wire rst_n,
+    input wire [7:0] zxuno_addr,
+    input wire zxuno_regrd,
+    input wire zxuno_regwr,
+    input wire [7:0] din,
+    output reg [7:0] dout,
+    output reg oe_n,
+    //---- DMA bus -----
+    output reg busrq_n,
+    input wire busak_n,
+    output reg [15:0] dma_a,
+    output reg dma_mreq_n,
+    output reg dma_iorq_n,
+    output reg dma_rd_n,
+    output reg dma_wr_n   
+    );
 
+    reg [1:0] modo = 2´b00;  // 00: apagado, 01: burst sin reinicio, 10: timed, sin reinicio, 11: timed, con reinicio    
+    reg [1:0] srcdest = 2´b00; // 00: memoria a memoria, 01: memoria a I/O, 10: I/O a memoria, 11: I/O a I/O
+    reg [15:0] src = 16´h0000, dst = 16´h0000;
+    reg [15:0] divisor = 16´h0000;
+    reg [15:0] contador = 16´h0000;
+    reg [2:0] estado = NODMA;
+    
+    always @(posedge clk) begin
+      if (rst_n == 1´b0) begin
+        modo <= 2´b00;
+        busrq_n <= 1´b1;
+        divisor <= 16´h0000;
+        contador <= 16´h0000;
+        estado <= NODMA;
+      end
+      else begin
+        case (estado)
+          NODMA:
+            begin
+              
+    
 
 endmodule
