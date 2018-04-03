84698a4c287a0efb1c5af0c9ed8ccd4b8ea26d28
avillena <avillena@zxuno>
2018-04-03 14:57:51 +0000

    Añado herramienta GOLDEN8
    git-svn-id: http://www.atc.us.es/svn/zxuno@1431 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

---
+++ software/upgrade/brick.asm
@@ -0,0 +1,138 @@
+                output  BRICK
+
+                include zxuno.inc
+
+              macro wreg  dir, dato
+                call    rst28
+                defb    dir, dato
+              endm
+
+                org     $2000           ; comienzo de la ejecución de los comandos ESXDOS
+
+Main            ld      bc, zxuno_port
+                out     (c), 0
+                inc     b
+                in      f, (c)
+                jp      p, Nonlock
+                call    Print
+                dz      'ROM not rooted'
+                ret
+Nonlock         ld      a, scandbl_ctrl
+                dec     b
+                out     (c), a
+                inc     b
+                in      a, (c)
+                ld      (normal+1), a
+                or      $80
+                out     (c), a
+                call    Print
+                dz      'Bricking...', 13
+
+                ld      a, $10
+                ld      hl, data
+                exx
+                ld      de, $0580
+                call    wrflsh
+                call    Print
+                dz      13, 'Done', 13
+                ld      bc, zxuno_port
+                ld      a, scandbl_ctrl
+                out     (c), a
+                inc     b
+normal          ld      a, 0
+                out     (c), a
+                ret
+
+Print           pop     hl
+                db      $3e
+Print1          rst     $10
+                ld      a, (hl)
+                inc     hl
+                or      a
+                jr      nz, Print1
+                jp      (hl)
+
+; ------------------------
+; Write to SPI flash
+; Parameters:
+;    A: number of pages (256 bytes) to write
+;   DE: target address without last byte
+;  HL': source address from memory
+; ------------------------
+wrflsh          ex      af, af'
+                xor     a
+wrfls1          wreg    flash_cs, 0     ; activamos spi, enviando un 0
+                wreg    flash_spi, 6    ; envío write enable
+                wreg    flash_cs, 1     ; desactivamos spi, enviando un 1
+                wreg    flash_cs, 0     ; activamos spi, enviando un 0
+                wreg    flash_spi, $20  ; envío sector erase
+                out     (c), d
+                out     (c), e
+                out     (c), a
+                wreg    flash_cs, 1     ; desactivamos spi, enviando un 1
+wrfls2          call    waits5
+                wreg    flash_cs, 0     ; activamos spi, enviando un 0
+                wreg    flash_spi, 6    ; envío write enable
+                wreg    flash_cs, 1     ; desactivamos spi, enviando un 1
+                wreg    flash_cs, 0     ; activamos spi, enviando un 0
+                wreg    flash_spi, 2    ; page program
+                out     (c), d
+                out     (c), e
+                out     (c), a
+                ld      a, $20
+                exx
+                ld      bc, zxuno_port+$100
+wrfls3          inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                inc     b
+                outi
+                dec     a
+                jr      nz, wrfls3
+                exx
+                wreg    flash_cs, 1     ; desactivamos spi, enviando un 1
+                ex      af, af'
+                dec     a
+                jr      z, waits5
+                ex      af, af'
+                inc     e
+                ld      a, e
+                and     $0f
+                jr      nz, wrfls2
+                ld      hl, wrfls1
+                push    hl
+waits5          wreg    flash_cs, 0     ; activamos spi, enviando un 0
+                wreg    flash_spi, 5    ; envío read status
+                in      a, (c)
+waits6          in      a, (c)
+                and     1
+                jr      nz, waits6
+                wreg    flash_cs, 1     ; desactivamos spi, enviando un 1
+                ret
+        
+rst28           ld      bc, zxuno_port + $100
+                pop     hl
+                outi
+                ld      b, (zxuno_port >> 8)+2
+                outi
+                jp      (hl)
+
+data            defb    $FF, $FF, $FF, $FF, $AA, $99, $55, $66
+                defb    $31, $E1, $FF, $FF, $32, $61, $80, $00
+                defb    $32, $81, $6B, $05, $32, $A1, $40, $00
+                defb    $32, $C1, $6B, $2A, $32, $E1, $00, $00
+                defb    $30, $A1, $00, $00, $33, $01, $31, $00
+                defb    $32, $01, $00, $1F, $30, $A1, $00, $0E
+                defb    $20, $00, $20, $00, $20, $00, $20, $00
+                defb    $00, $00, $00, $00, $00, $00, $00, $00
