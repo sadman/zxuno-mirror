952da39cac06e16d0ee90c3cbbb10d86b292d948
rodriguj <rodriguj@zxuno>
2020-03-05 14:28:12 +0000

    Arreglado el Timex HiRes, que se escogorció al principio de EXP27
    git-svn-id: http://www.atc.us.es/svn/zxuno@1537 c9f0b58d-52e7-4fa3-91d2-c85e4586ca94

--- cores/spectrum_v2_spartan6/exp27/common/ula_radas.v
+++ cores/spectrum_v2_spartan6/exp27/common/ula_radas.v
@@ -220,23 +220,28 @@ module ula_radas (
     reg [7:0] BitmapSerializer = 8'h00;
     wire SerialOutput = BitmapSerializer[7];
     always @(posedge sysclk) begin
-      if (clk7en) begin
-        if (SerializerLoad)
+      if (clk7en == 1'b1) begin
+        if (SerializerLoad == 1'b1)
            BitmapSerializer <= BitmapData;
         else
            BitmapSerializer <= {BitmapSerializer[6:0],1'b0};
       end
     end
     
+    reg clkhalf14 = 1'b0;
+    always @(posedge sysclk)
+      if (clk14en == 1'b1)
+        clkhalf14 <= ~clkhalf14;    
+        
     // BitmapSerializerHR register
     reg [15:0] BitmapSerializerHR = 8'h00;
     wire SerialOutputHR = BitmapSerializerHR[15];
     always @(posedge sysclk) begin
-      if (clk14en) begin
-        if (SerializerLoad)
-           BitmapSerializerHR <= {BitmapData,AttrData};
+      if (clk14en == 1'b1) begin
+        if (SerializerLoad == 1'b1 && clkhalf14 == 1'b1)
+          BitmapSerializerHR <= {BitmapData,AttrData};
         else
-           BitmapSerializerHR <= {BitmapSerializerHR[14:0],1'b0};
+          BitmapSerializerHR <= {BitmapSerializerHR[14:0],1'b0};
       end
     end
     
